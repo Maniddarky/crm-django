1f87c612c179f5dcfe0dc2faea42bda2f7769369
Maniddarky <42319136+Maniddarky@users.noreply.github.com>
2020-06-07 13:23:36 +0530

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_int')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static_int/images')
