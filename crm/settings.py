dd47f52b1cf0b49a4a264efa0785703520779b9a
Maniddarky <42319136+Maniddarky@users.noreply.github.com>
2020-06-07 12:55:01 +0530

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -137,12 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
